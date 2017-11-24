c4e7d451669fede521bfc02bc9c2bba1191af6c1
Jose Alberto Colin Perez <jose.colin@globant.com>
2017-11-24 16:25:43 -0600

    some adds

--- src/main/java/jose_colin_appium/MainActivity.java
+++ src/main/java/jose_colin_appium/MainActivity.java
@@ -1,63 +1,50 @@
 package jose_colin_appium;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 
-public class MainActivity extends BaseActivity{
+public class MainActivity extends BaseActivity {
 
-	private final String DISPLAY_TEXT_BTN="io.selendroid.testapp:id/visibleButtonTest";
-	private final String INPUT_TEXT="io.selendroid.testapp:id/my_text_field";
-	private final String BTN_LONG="io.selendroid.testapp:id/buttonTest";
-	private final String CHECK_BOX="io.selendroid.testapp:id/input_adds_check_box";
-	private final String DIALOG_BTN="android:id/button1";
-	
-	
-	
-	
+	private final String DISPLAY_TEXT_BTN = "io.selendroid.testapp:id/visibleButtonTest";
+	private final String INPUT_TEXT = "io.selendroid.testapp:id/my_text_field";
+	private final String BTN_LONG = "io.selendroid.testapp:id/buttonTest";
+	private final String CHECK_BOX = "io.selendroid.testapp:id/input_adds_check_box";
+	private final String DIALOG_BTN = "android:id/button1";
+
 	public MainActivity(AndroidDriver<MobileElement> driver) {
 		super(driver);
 	}
 
 	public void inicio() {
-//		MobileElement btn=getDriver().findElement(By.id(DISPLAY_TEXT_BTN));
-//		
-//		btn.click();
-//		
-//		MobileElement input=getDriver().findElement(By.id(INPUT_TEXT));
-//		
-//		input.sendKeys("holaaa");
-//		
-//		MobileElement btn2=getDriver().findElement(By.id(BTN_LONG));
-//		
-//		TouchAction action = new TouchAction(getDriver());
-//		action.longPress(btn2).release().perform();
-//		MobileElement btn3=getDriver().findElement(By.id(DIALOG_BTN));
-////		getWait().until(ExpectedConditions.visibilityOf(btn3));
-//		btn3.click();
-//		
-//		
-//		
-//		MobileElement btn4=getDriver().findElement(By.id(CHECK_BOX));
-//		
-//		btn4.click();
-		
-//		MobileElement view = getDriver().findElement(By.className("android.widget.LinearLayout"));
-		
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		 TouchAction touchAction = new TouchAction(driver).press(240,390)
-	                .moveTo(240, 75).release();
-	        driver.performTouchAction(touchAction);
-	        
-	        System.out.println("aa");
+		MobileElement btn = driver.findElement(By.id(DISPLAY_TEXT_BTN));
+
+		btn.click();
+
+		MobileElement input = driver.findElement(By.id(INPUT_TEXT));
+
+		input.sendKeys("holaaa");
+
+		MobileElement btn2 = driver.findElement(By.id(BTN_LONG));
+		TouchAction action = new TouchAction(driver);
+		action.longPress(btn2).release().perform();
+		MobileElement btn3 = driver.findElement(By.id(DIALOG_BTN));
+		wait.until(ExpectedConditions.visibilityOf(btn3));
+		btn3.click();
+
+		MobileElement btn4 = driver.findElement(By.id(CHECK_BOX));
+
+		btn4.click();
+
+		MobileElement view = driver.findElement(By.className("android.widget.LinearLayout"));
+
+		TouchAction touchAction = new TouchAction(driver).press(240, 390).moveTo(240, 75).release();
+		driver.performTouchAction(touchAction);
+
+		System.out.println("aa");
 	}
 
 }
